60c4e82ee73e2f1e20ef73195e822996a99a7363
Kay Apple <kayashiro.huy.ho@gmail.com>
2022-07-26 23:17:25 +0200

    created a button

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.bizcard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +32,8 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CreateBizCard()
                 }
             }
         }
@@ -30,14 +41,103 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            backgroundColor = Color.White,
+            elevation = 4.dp
+        ) {
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider()
+                Column(
+                    modifier = Modifier
+                        .padding(5.dp)
+                ) {
+                    CreateInfo()
+                }
+                Column(
+                    modifier = Modifier
+                        .height(300.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(
+                        modifier = Modifier
+                            .padding(20.dp),
+                        onClick = { Log.d("Clicked", "CreateBizCard: Clicked") }
+                    ) {
+                        Text(
+                            text = "Portfolio",
+                            style = MaterialTheme.typography.button
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
+
+@Composable
+private fun CreateInfo() {
+    Text(
+        text = "Kay Ho.",
+        style = MaterialTheme.typography.h4,
+        color = MaterialTheme.colors.primaryVariant
+    )
+    Text(
+        text = "The best developer in making.",
+        style = MaterialTheme.typography.subtitle1,
+    )
+    Text(
+        modifier = Modifier.padding(3.dp),
+        text = "kayshiro@live.no",
+        style = MaterialTheme.typography.subtitle1
+    )
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(
+            0.5.dp,
+            Color.LightGray
+        ),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f),
+    ) {
+        Image(
+            modifier = Modifier
+                .size(135.dp)
+                .background(color = Color.White),
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile Image",
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BizCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
